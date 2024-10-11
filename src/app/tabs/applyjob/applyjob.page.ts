8992469e080ee46b1445014d7214b035d0fc83a1
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-10-11 18:01:52 +0300

    update

--- src/app/tabs/applyjob/applyjob.page.ts
+++ src/app/tabs/applyjob/applyjob.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonText, IonButtons, IonBackButton,IonThumbnail, IonItem, IonChip, IonList, IonInput,IonSelectOption,IonSelect, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -10,23 +10,30 @@ import Swal from 'sweetalert2';
   templateUrl: './applyjob.page.html',
   styleUrls: ['./applyjob.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonList, IonChip, IonItem, IonBackButton, IonButtons, IonText,IonThumbnail, IonLabel, IonContent, IonHeader,IonSelectOption ,IonTitle, IonToolbar, CommonModule,IonSelect, FormsModule]
+  imports: [IonButton, IonInput, IonList,ReactiveFormsModule, IonChip, IonItem, IonBackButton, IonButtons, IonText,IonThumbnail, IonLabel, IonContent, IonHeader,IonSelectOption ,IonTitle, IonToolbar, CommonModule,IonSelect, FormsModule]
 })
 export class ApplyjobPage implements OnInit {
   jobPost: string = '';
   salary:string='';
   type:string=''
-  constructor(private route: ActivatedRoute,private router: Router) { }
-
+  constructor(private route: ActivatedRoute,private router: Router) {
+    this.initForm();
+   }
+  form!: FormGroup;
   ngOnInit() {
     this.jobPost = this.route.snapshot.paramMap.get('jobPost') || '';
     this.type=this.route.snapshot.paramMap.get('type') || '';
     this.salary=this.route.snapshot.paramMap.get('salary') || '';
+   
   }
 
   open(){
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }else{
     Swal.fire({
-      title: 'Logging In',
+      title: 'Submitting',
       allowEscapeKey: false,
       allowOutsideClick: false,
       timer: 2000,  // Display this for 2 seconds
@@ -39,7 +46,7 @@ export class ApplyjobPage implements OnInit {
       if (result.dismiss === Swal.DismissReason.timer) {
         // Display 'Finished!' SweetAlert
         Swal.fire({
-          title: 'Logged In Successfully!',
+          title: 'Submitted Successfully!',
           icon: 'success',  // Use 'icon' instead of 'type'
           timer: 2000,
           showConfirmButton: false,
@@ -49,7 +56,14 @@ export class ApplyjobPage implements OnInit {
           this.router.navigateByUrl('/tabs/home');
         });
     
-      }})}
+      }})}}
 
+      initForm() {
+        this.form = new FormGroup({
+          email: new FormControl('', [Validators.required, Validators.email]),
+          name: new FormControl('', [Validators.required]),
+          phoneNumber:new FormControl('', [Validators.required]),
+        });
+      }
   }
 
